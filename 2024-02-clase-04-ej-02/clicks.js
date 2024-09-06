9b4826e9aeed13cf1baf06959849631f51cbb695
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2024-09-05 22:28:58 -0300

    implemente funciones para mostrar datos en el html

--- 2024-02-clase-04-ej-02/clicks.js
+++ 2024-02-clase-04-ej-02/clicks.js
@@ -1,11 +1,56 @@
 function cargarAlumno() {
-    console.log("TODO");
+    // Pido el nombre del alumno
+    alumno = validarString("Ingrese el nombre del alumno");
 }
 
 function cargarMateria() {
-    console.log("TODO");
+    // Pido la materia y valido
+    let materia = validarString("Ingrese el nombre de la materia");
+    // Guardo la materia en su array
+    materias.push(materia);
+    // Pido nota y valido
+    let nota = validarNota(`Ingrese la nota de final de ${materia}`);
+    // Guardo la nota en su array
+    notas.push(nota);
 }
 
 function mostrar() {
-    console.log("TODO");
+    /**
+     * <h1>${alumno}</h1>
+     * <h2>Materias</h2>
+     * <ul class="list-group">
+     *      <li class="list-group-item">${materia} - ${nota}</li>
+     *      <li class="list-group-item">${materia} - ${nota}</li>
+     *      <li class="list-group-item">${materia} - ${nota}</li>
+     *      <li class="list-group-item">${materia} - ${nota}</li>
+     * </ul>
+     */
+
+    // Encuentro el container (div)
+    const container = document.querySelector(".container");
+    // Limpio el container
+    container.innerHTML = "";
+    // Creo el h1 con el nombre del alumno
+    container.innerHTML += `<h1>${alumno}</h1>`;
+    container.innerHTML += `<h2>Materias</h2>`;
+    // Creo la lista (se cierra automáticamente)
+    container.innerHTML += `<ul class="list-group">`;
+    // Encuentro la lista que generé recién
+    const ul = document.querySelector("ul");
+    // Agrego las materias
+    
+    // Con un for normal
+    // for(let i = 0; i < materias.length; i++) {
+    //     ul.innerHTML += `<li class="list-group-item">${materias[i]} - ${notas[i]}</li>`;
+    // }
+
+    // Con un for in
+    for(let i in materias) {
+        ul.innerHTML += `<li class="list-group-item">${materias[i]} - ${notas[i]}</li>`;
+    }
+
+    // Con un for of (a medias anda)
+    // for(let materia of materias) {
+    //     ul.innerHTML += `<li class="list-group-item">${materia}</li>`;
+    // }
 }
--- 2024-02-clase-04-ej-02/index.js
+++ 2024-02-clase-04-ej-02/index.js
@@ -1,17 +1,19 @@
+// Variable para guardar el nombre del alumno
+let alumno = "Fulano de tal";
 // Variables para almacenar los datos de las materias
 let materias = [], notas = [];
 
-// Pido el nombre del alumno
-const alumno = validarString("Ingrese el nombre del alumno");
+// // Pido el nombre del alumno
+// const alumno = validarString("Ingrese el nombre del alumno");
 
-do {
-    // Pido la materia y valido
-    let materia = validarString("Ingrese el nombre de la materia");
-    // Guardo la materia en su array
-    materias.push(materia);
-    // Pido nota y valido
-    let nota = validarNota(`Ingrese la nota de final de ${materia}`);
-    // Guardo la nota en su array
-    notas.push(nota);
+// do {
+//     // Pido la materia y valido
+//     let materia = validarString("Ingrese el nombre de la materia");
+//     // Guardo la materia en su array
+//     materias.push(materia);
+//     // Pido nota y valido
+//     let nota = validarNota(`Ingrese la nota de final de ${materia}`);
+//     // Guardo la nota en su array
+//     notas.push(nota);
 
-} while(confirm("Otra materia y nota para cargar?"));
+// } while(confirm("Otra materia y nota para cargar?"));
