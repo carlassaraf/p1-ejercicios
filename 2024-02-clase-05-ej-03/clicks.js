7e33ee7a123156bd12b0b07fa7f1500700609b62
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2024-09-17 22:23:53 -0300

    agrego funcion de mostrar en html

--- 2024-02-clase-05-ej-03/clicks.js
+++ 2024-02-clase-05-ej-03/clicks.js
@@ -3,6 +3,8 @@ function cargarNombre() {
     const nombre = validarString("Ingrese el nombre del alumno");
     // Paso el nombre al objeto alumno
     alumno.nombre = nombre;
+    // Otra posibilidad
+    // alumno["nombre"] = nombre;
 }
 
 function cargarMateria() {
@@ -20,6 +22,25 @@ function cargarMateria() {
 }
 
 function cargarAlumno() {
+    // Agrego una funcion como propiedad
+    alumno.toHTML = function() {
+        // Variable de string vacia
+        let str = "";
+        // Agrego h1 con nombre del alumno
+        str += `<h1>${this.nombre}</h1>\n`;
+        str += `<h2>Materias</h2>\n`;
+        // Creo un string auxiliar
+        str +=  `<ul class="list-group">\n`;
+        // Recorro las materias del alumno
+        for(let materia of this.materias) {
+            // Armo el li
+            str += `<li class="list-group-item">${materia.nombre} - ${materia.nota}</li>\n`
+        }
+        // Cierro el ul
+        str += `</ul>`;
+        // Devuelvo str
+        return str;
+    }
     // Subo al alumno a la lista
     alumnos.push(alumno);
     // Inicializar un objeto nuevo
@@ -29,5 +50,50 @@ function cargarAlumno() {
 }
 
 function mostrarLista() {
-    // TODO
+    /**
+     * <h1>${alumno}</h1>
+     * <h2>Materias</h2>
+     * <ul class="list-group">
+     *      <li class="list-group-item">${materia} - ${nota}</li>
+     *      <li class="list-group-item">${materia} - ${nota}</li>
+     *      <li class="list-group-item">${materia} - ${nota}</li>
+     *      <li class="list-group-item">${materia} - ${nota}</li>
+     * </ul>
+     */
+
+    // Traigo el primer elemento que sea de clase container
+    const div = document.querySelector(".container");
+    // Limpio HTML
+    div.innerHTML = "";
+
+    // Recorro la lista de alumnos
+
+    // for(let i in alumnos) {
+    //     // Agrego h1 con nombre de alumno
+    //     div.innerHTML += `<h1>${alumnos[i].nombre}</h1>`;
+    // }
+
+    // Sin propiedad .mostrar
+    // for(let alumno of alumnos) {
+    //     // Agrego h1 con nombre del alumno
+    //     div.innerHTML += `<h1>${alumno.nombre}</h1>`;
+    //     div.innerHTML += `<h2>Materias</h2>`;
+    //     // Creo un string auxiliar
+    //     let str =  `<ul class="list-group">\n`;
+    //     // Recorro las materias del alumno
+    //     for(let materia of alumno.materias) {
+    //         // Armo el li
+    //         str += `<li class="list-group-item">${materia.nombre} - ${materia.nota}</li>\n`
+    //     }
+    //     // Cierro el ul
+    //     str += `</ul>`;
+    //     // Paso el string al html
+    //     div.innerHTML += str;
+    // }
+
+    // Aprovechando la propiedad .mostrar de alumno
+    for(let alumno of alumnos) {
+        // Le paso al div el HTML que genera el propio objeto
+        div.innerHTML += alumno.toHTML();
+    }
 }
